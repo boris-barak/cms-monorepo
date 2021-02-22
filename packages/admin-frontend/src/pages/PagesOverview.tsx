925b64a9471ea34d7f730e5eb6cbbf6d46e828fb
Boris Barak <boris.barak@adverity.com>
2021-02-22 20:51:31 +0100

    page overview tweaks

--- packages/admin-frontend/src/pages/PagesOverview.tsx
+++ packages/admin-frontend/src/pages/PagesOverview.tsx
@@ -3,9 +3,14 @@ import { getAllPages } from '../api/content-service';
 import { useQuery } from 'react-query';
 import { Heading } from '../design-components/Heading';
 import { Grid, GridBody, GridHeader, GridHeaderColumn, GridItem, GridRow } from '../design-components/Grid';
+import { openInNewTab } from '../utils';
+import { Button } from '../design-components/Button';
 
 export const PagesOverview = () => {
     const { data: pages } = useQuery('pages', getAllPages);
+
+    const handleOpen = (url: string) => openInNewTab(`http://localhost:3000/${url}`);
+    const handleEdit = (url: string) => openInNewTab(`http://localhost:3000/${url}`);
 
     return (
         <>
@@ -15,35 +20,19 @@ export const PagesOverview = () => {
                     <GridHeaderColumn border>Title</GridHeaderColumn>
                     <GridHeaderColumn border>URL</GridHeaderColumn>
                     <GridHeaderColumn border>Keywords</GridHeaderColumn>
-                    <GridHeaderColumn border>Web</GridHeaderColumn>
+                    <GridHeaderColumn border>Actions</GridHeaderColumn>
                 </GridHeader>
                 <GridBody>
                     {pages?.map((page) => (
-                        <React.Fragment key={page.url}>
-                            <GridRow>
-                                <GridItem border>{page.title}</GridItem>
-                                <GridItem border>/{page.url}</GridItem>
-                                <GridItem border>{page.keywords?.join(',')}</GridItem>
-                                <GridItem border>
-                                    <a className="App-link" href={`http://localhost:3000/${page.url}`} target="_blank">
-                                        See
-                                    </a>
-                                </GridItem>
-                            </GridRow>
-                            <GridRow>
-                                <GridItem border colSpan={3} htmlContent={page.content} />
-                                <GridItem border>
-                                    <a className="App-link" href={`http://localhost:3000/${page.url}`} target="_blank">
-                                        Edit
-                                    </a>
-                                </GridItem>
-                            </GridRow>
-                            <GridRow>
-                                <GridItem colSpan={4}>
-                                    <hr />
-                                </GridItem>
-                            </GridRow>
-                        </React.Fragment>
+                        <GridRow key={page.url}>
+                            <GridItem border>{page.title}</GridItem>
+                            <GridItem border>/{page.url}</GridItem>
+                            <GridItem border>{page.keywords?.join(',')}</GridItem>
+                            <GridItem border>
+                                <Button onClick={() => handleOpen(page.url)}>Open</Button>
+                                <Button onClick={() => handleEdit(page.url)}>Edit</Button>
+                            </GridItem>
+                        </GridRow>
                     ))}
                 </GridBody>
             </Grid>
